0d6dba7a0c558448c3d05993c99d5b982546c90b
brooken1516 <brooken1516@gmail.com>
2025-09-05 16:05:27 -0600

    Finished lesson 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,11 +6,10 @@ class Program
     {
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-        
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
         {
-            Console.WriteLine($"Your name is {last}, {first} {last}. ");
+            Console.WriteLine($"Your name is {last}, {first} {last}.");
         }
     }
 }
