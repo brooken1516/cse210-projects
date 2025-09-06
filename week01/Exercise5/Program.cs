3ed632b8559f49083ca791facb8b6d7a8749c52c
brooken1516 <brooken1516@gmail.com>
2025-09-06 16:19:42 -0600

    Finished assigment #5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        welcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = NumberSquared(number);
+        displayResult(userName, squaredNumber);
+    }
+    static void welcome()
+    {
+        Console.WriteLine("Welcome to the Program! ");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userInput = Console.ReadLine();
+        return userInput;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        return number;
+    }
+    static int NumberSquared(int number)
+    {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+    static void displayResult(string userName, int numberSquared)
+    {
+        Console.Write($"{userName}, the square of your favorite number is {numberSquared}. ");
     }
 }
