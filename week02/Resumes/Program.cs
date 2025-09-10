795119d68a65e4826ac6a91be6b059827d679e53
brooken1516 <brooken1516@gmail.com>
2025-09-10 09:36:47 -0600

    Completed w02 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2015;
+        job2._endYear = 2020;
+
+        Resume myresume = new Resume();
+        myresume._fullName = "Brooke Grandberry";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+        myresume.Display();
     }
 }
