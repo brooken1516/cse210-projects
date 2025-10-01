d21c14d319dd0843cd40b52f674b7707fd21577c
brooken1516 <brooken1516@gmail.com>
2025-10-01 12:19:21 -0600

    Completed Assignment

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MathAssignment:Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems)
+    :base(studentName, topic)
+    {
+        _textbookSection = textBookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeWorkList()
+    {
+        return "Section: " + _textbookSection  + " Problems: " + _problems;
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("George Washington", "Social Sciences");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Bastian Grandberry", "Addition", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeWorkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Atreyu Grandbery", "Sentences", "Sentence Structure");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
